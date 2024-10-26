304d6ba4ab9f0ccae229c065446b8b9d9df00aed
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-10-26 15:15:11 -0500

    Add license verification alarm

--- src/scripts/background/index.ts
+++ src/scripts/background/index.ts
@@ -1,10 +1,26 @@
 /* eslint-disable no-console */
 
 import { browser } from "webextension-polyfill-ts"
+import { verifyLicenseKey } from "@/utils/license";
+import { get } from "http";
 
 browser.runtime.onInstalled.addListener((): void => {
   console.log("🦄", "extension installed")
 })
+
+browser.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name === 'CHECK-LICENSE') {
+    console.log('Checking if there is a valid license...');
+    await verifyLicenseKey();
+  }
+});
+
+browser.alarms.create('CHECK-LICENSE', {
+  when: Date.now() + 10,
+  periodInMinutes: 1,
+}) // validate license every 30 mins
+
+
 
 // Please remove default_popup from manifest.json
 // And you can enable this code to open to communicate with content
