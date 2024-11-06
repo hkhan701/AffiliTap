f071a1c4ebff17ca1c549bff41249757637d7f57
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-11-06 00:32:34 -0600

    Add utils.ts file for utility functions and helper methods

---
+++ src/utils/utils.ts
@@ -0,0 +1,51 @@
+import { browser } from "webextension-polyfill-ts";
+import { getPage } from "@/utils/urls";
+
+export async function md5Hash(message: string): Promise<string> {
+    const msgUint8 = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
+    return Array.from(new Uint8Array(hashBuffer))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
+
+export const handlePurchaseRedirect = () => {
+    window.open("https://affilitap.lemonsqueezy.com/checkout", "_blank"); // Update with real purchase link
+}
+
+export const handleAddTemplate = () => {
+    browser.tabs.create({
+        url: browser.runtime.getURL(getPage("index.html")),
+    });
+}
+
+export const getShortUrl = async (trackingId: string) => {
+    const tabs = await browser.tabs.query({ active: true, lastFocusedWindow: true });
+    const activeTab = tabs[0];
+    const longUrl = activeTab.url;
+
+    const obj = {
+        p_parameter: 'SS v2',
+        test_name: 'SiteStripe V3.0',
+        toolCreation: 'SS',
+        adUnitType: 'TEXT',
+        adUnitDescription: 'Product links Text only link',
+        destinationType: 'ASIN',
+        marketplaceId: '1',
+        store: trackingId,
+        tag: trackingId,
+        adUnitSubType: 'ShortLinks',
+        linkCode: 'sl1',
+        createTime: new Date().getTime(),
+    };
+
+    const linkId = await md5Hash(JSON.stringify(obj));
+    const linkToFetch = `https://www.amazon.ca/associates/sitestripe/getShortUrl?longUrl=`
+    +encodeURIComponent(`${longUrl}&linkCode=sl1&tag=${trackingId}&linkId=${linkId}`)
+     + `&language=en_CA&ref_=as_li_ss_tl&marketplaceId=7`
+
+    const res = await fetch(linkToFetch);
+    const links = await res.json();
+    console.log("The links are: ", links);
+    return links.shortUrl;
+};
